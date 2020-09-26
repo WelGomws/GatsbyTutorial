1b6b7b6e91840d0aa24e554db7f31cab0ab2948e
WelGomws <wellingtond.gomes@hotmail.com>
2020-09-26 17:11:26 -0300

    feat(pages/index): add new texts

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,12 +4,26 @@ import SEO from "../components/seo.jsx"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" description="Welcome!!" />
-    <div className="container" style={{ display: "flex" }}>
+    <SEO title="Roi" description="Welcome!!" />
+    <div className="container" style={{ display: "flex", fontSize: "16px" }}>
       <div className="row">
-        <div className="col-12 py-4">
-          <h1>Home</h1>
-          <p>Welcome to your new Gatsby site.</p>
+        <div className="col-12 py-5">
+          <h1>Oi mi amor!</h1>
+          <p>
+            Comecei a estudar esse criador de site quando precisei criar o site
+            da empresa que trabalho, acompanhando os videos tutoriais do
+            youtube, ficou um site bem bacana no projeto, pensei porque não
+            adaptar pra uma surpresinha pra minha namoradinha né?
+          </p>
+          <p>
+            Estou atualizando o site com a sua escolhas de cores em, te pedindo
+            ajuda sem você perceber bobinha. Não sei o que escrever aqui, você
+            sabe que não sou bom com palavras...
+          </p>
+          <p>
+            Deixo claro aqui todo meu amor por você meu bbxinho mais lindo, me
+            desculpa por cada erro meu, por eu ser seu estresse diário rsrs, te amo demais fofura. {"<3"}
+          </p>
         </div>
       </div>
     </div>
