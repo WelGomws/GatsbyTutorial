82044e6f98a4b179170452a54b5ca4f1f04c7474
WelGomws <wellingtond.gomes@hotmail.com>
2020-09-26 17:09:00 -0300

    feat(pages): add style and text

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,10 +5,15 @@ import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404" />
+    <div className="container" style={{ display: "flex", fontSize: "16px" }}>
+      <div className="row">
+        <div className="col-12 py-5">
+          <h1>Opa meu lindo!</h1>
+          <p>A página que você está procurando não exite.</p>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
-
 export default NotFoundPage
