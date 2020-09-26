7f0cac9d834ff3c8b2be6af212cbc1288ad2caea
WelGomws <wellingtond.gomes@hotmail.com>
2020-09-26 17:10:40 -0300

    feat(styles/pages): add phone icon

---
+++ src/styles/pages/contatoStyles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+import Icons from "../../components/icons"
+
+export const EmailWrapper = styled(Icons.Email)`
+  color: #f5a9bc;
+  width: 35px;
+  margin: 5px;
+`
+export const PhoneWrapper = styled(Icons.Phone)`
+  color: #f5a9bc;
+  width: 35px;
+  margin: 5px;
+`
