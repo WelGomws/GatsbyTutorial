b6a29c586fd05051a9934ddde8e52d461c3eca4c
WelGomws <wellingtond.gomes@hotmail.com>
2020-10-18 00:50:26 -0300

    feat(styles): add font-family

--- src/styles/globalStyled.js
+++ src/styles/globalStyled.js
@@ -1,6 +1,10 @@
 import { createGlobalStyle } from "styled-components"
 
 const GlobalStyles = createGlobalStyle`
+  body {
+    font-family: 'Caveat', cursive;
+  }
+
   h1 {
     color: #F5A9BC;
     margin-bottom: 16px;
