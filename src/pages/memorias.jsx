e30612754c5c158f6d7e2c8dcb86e2f2e15dcffc
WelGomws <wellingtond.gomes@hotmail.com>
2020-10-18 00:50:11 -0300

    feat(pages/memorias): add new texts

---
+++ src/pages/memorias.jsx
@@ -0,0 +1,285 @@
+import React from "react"
+import Layout from "../components/Layout"
+import Cardv2 from "../components/Cardv2"
+import SEO from "../components/seo.jsx"
+import * as S from "../styles/pages/motivosStyle"
+
+const Memorias = () => {
+  const text =
+    "Aqui vai algumas memórias nossas que amei passar ao seu lado! ❤️"
+
+  return (
+    <Layout>
+      <SEO title="Memorias" />
+      <div className="container">
+        <div className="row">
+          <div className="col-12 py-5">
+            <h1>Memorias</h1>
+            <h2 style={{fontSize:'18px'}}>{text}</h2>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <S.Grid>
+              <div>
+                <Cardv2
+                  nameImage="01"
+                  titleCard="Primeira foto?"
+                  textCard="Nossa primeira fotinhas juntos (eu acho), primeira de muitas né peste, não para de tirar foto 😡 kakakaka"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="02"
+                  titleCard="Pizzaria"
+                  textCard="Primeira vez em um rodizio de pizza com minha namoradinha 😁"
+                />
+              </div>
+          
+              <div>
+                <Cardv2
+                  nameImage="04"
+                  titleCard="Meu porto seguro"
+                  textCard="Muito obrigado por ser meu porto seguro, por me apoiar em tudo que faço, por recarregar minhas energias 🥰"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="05"
+                  titleCard="Saudades de um shopping"
+                  textCard="Saudades de um shoppingzinho né minha vida? Também tô, quando tudo isso acabar juro que a gente vai ❤️"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="03"
+                  titleCard="Primeira viagem"
+                  textCard="Se loco parceria ilha bela foi bom demais fala tu, a primeira viagem de muitas na nossa vidinha fofura 🤩"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="06"
+                  titleCard="Mi Tompero"
+                  textCard="Te amo por sempre querer tirar foto com esse gato fofo e tonto que tanto enchemos o saco e que não aguenta nem ver nosso rosto mais 😾"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="08"
+                  titleCard="Paisagem"
+                  textCard="Sempre querendo tirar foto quando ve uma paisagem bonita né, mas todos sabemos que a melhor paisagem aos meus olhos é você 🥰"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="07"
+                  titleCard="Festinha"
+                  textCard="Adora me arrastar pras festas mesmo eu não gostando de gente, tóxica 🤪"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="09"
+                  titleCard="Ta se achando né?"
+                  textCard="Se achando a gostosona né? Parabéns eu também acho 👍"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="10"
+                  titleCard="Surtada?"
+                  textCard="Briga e briga por causa da moto mas adora dar um peão nela né 🤪🤪🤪🤪🤪 essa foto é muito boa KAKAKAKAKAAKK"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="13"
+                  titleCard="Kazinho bala"
+                  textCard="Nossa primeira foto no kazinho bala mais conhecido como Relâmpago Mcqueen e obviamente o nosso piquinique no riachão rsrs 🚗"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="12"
+                  titleCard="Fofinha"
+                  textCard="Tão fofinha e linda distraida iti malia meu deus do céu que coisa linda 🥰"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="11"
+                  titleCard="Fofinha 2.0"
+                  textCard="Nada a declarar pra essa fofura 🥰"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="14"
+                  titleCard="Mi tompero 2.0"
+                  textCard="Mi tompero segurando o contole do firestick da amazon (patriocina nois), muito bom KAKAKAKAKAKAAKAK"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="15"
+                  titleCard="Gatos em"
+                  textCard="Se loco cachorrera nois é gato em, tamo junto parceria nois que voa bruxona 😗✌️"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="19"
+                  titleCard="Planet"
+                  textCard="Me arrastar pra tabacaria o alge de rolê adolescente se loco, mas um dia sai a fumaça de bolinha, foco força e fé 😔✊"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="20"
+                  titleCard="Solzin"
+                  textCard="Ama tomar um solzinho de manhãzinha e isso é tão bom misericórdia catatau 🥰☀️"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="18"
+                  titleCard="Espelho"
+                  textCard="Ama tirar uma fotinha em um espelho misericórdia em parceira, ta maluco tio 🤪, mas a gente é gato em se loco"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="16"
+                  titleCard="Mimi"
+                  textCard="Pegar a mimi pra tirar foto mesmo sendo a gata mais brava da sua casa e a que mais odeia humanos KAKAKAKAKAK"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="21"
+                  titleCard="Book da Mimi"
+                  textCard="Book de fotos da mimi grávida no meio da amoreira, sei lá o nome disso KAKAKAKAKAKAK, ninguém sabe que ela estava tentando fugir da gente..."
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="17"
+                  titleCard="Ódio"
+                  textCard="Podemos ver o ódio e a vontade de matar a gente nos olhos do felino mas a fofura é tão grnade que a gente nem liga, somente apenas 🤷‍♂️"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="22"
+                  titleCard="Outback"
+                  textCard="Nossa comemoração de 2 aninhos de namoro, se loco mano esse camarão tava bom demais, quando acabar a pandemia vamos por favor nunca te pedi nada"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="23"
+                  titleCard="Lele"
+                  textCard="Lelezinho não podia faltar nesse meio que album de fotinhas né KAKAKAKAKAKA titicos"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="24"
+                  titleCard="Buque de flor"
+                  textCard="No Brasil ta chovendo mulher e o que elas querem é buque de flor, buque de flor 🎵"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="25"
+                  titleCard="Buque de flor 2.0"
+                  textCard="Acho que ficou bravinha por causa da musica KAKAKAKAKAKA zoeira"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="26"
+                  titleCard="Se achando de novo?"
+                  textCard="Se achando a gostosona de novo mas agora na praia né? Parabéns eu também acho de novo, é isto 👍"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="27"
+                  titleCard="Salve quebrada"
+                  textCard="As famosas julietes que compramos na praia só por meme mas adoramos tirar fotos com elas KAKAKAKAKAKAK"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="28"
+                  titleCard="Natal"
+                  textCard="Não pode ver um ledzinho ou um pisca pisca que já quer ir tirar foto fofinha 🎄"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="30"
+                  titleCard="Ponte"
+                  textCard="Ponte ponte, ponte ponte, assim é bem melhor 🎵, voltando do rolezinho que já nem lembro mais qual é porque eu só queria dormir bicho"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="29"
+                  titleCard="Niver do Lele"
+                  textCard="Niverzinho de dino do lelezinho menino brabo que quer roubar minha namoradinha mas ainda considero ele pakas KAKAKAKAKAKAKAKAKAK"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="31"
+                  titleCard="???"
+                  textCard="Não sei do que tu tava rindo mas deve ter sido muito engraçado pra você porque minha cara ta só ??????????? KAKAKAKAK"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="36"
+                  titleCard="18tão"
+                  textCard="Jantar de 18 da minha namoradinha linda, nem parece que me arrastou pro supra depois com o carro DO SEU PAI né demonio"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="32"
+                  titleCard="Patrões"
+                  textCard="Nois tava brabo esse dia pode falar se loco que casalzão da porra"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="34"
+                  titleCard="Piscininha amor"
+                  textCard="Saudades de ir em uma piscina né, apesar que se bater um ventinho tu já fala que ta frio e não entra KAKAKAKAKAKA"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="33"
+                  titleCard="Piscininha 2.0"
+                  textCard="Saudades de ir em uma chacarazinha né, corona do caralho vai embora micobrio do caralho fia da puta 😡😡😡"
+                />
+              </div>
+              <div>
+                <Cardv2
+                  nameImage="35"
+                  titleCard="Caiaque"
+                  textCard="Primeio passeio de caiaque com minha namoradinha que foi escondido da mãe dela então não mostre esse foto pra ela porque não sei se você já contou ou não KAKAKAKAKA"
+                />
+              </div>
+
+
+        </S.Grid>
+      </div>
+    </Layout>
+  )
+}
+
+export default Memorias
