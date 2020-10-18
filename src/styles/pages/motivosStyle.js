732d1eaf963da65e022beef2e14ec6be0f217c00
WelGomws <wellingtond.gomes@hotmail.com>
2020-10-17 22:20:24 -0300

    feat(pages/motivos): add new cards and style in grid

---
+++ src/styles/pages/motivosStyle.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const Grid = styled.div`
+  display: grid;
+  padding: 0;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 10px;
+  align-items: center;
+`
