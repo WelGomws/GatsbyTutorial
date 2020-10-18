78dd3767951866fe34626e369b60c2a208ba71cf
WelGomws <wellingtond.gomes@hotmail.com>
2020-10-17 22:18:49 -0300

    feat(components/footer): add style in new file

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import * as Styled from "./styled"
+import Container from '../Container'
 
 const Footer = () => {
   const {
@@ -27,30 +28,32 @@ const Footer = () => {
     }
   `)
   return (
-    <div style={{ background: "#A9D0F5", borderRadius: "16px 16px 0px 0px" }}>
-      <div className="container">
-        <div className="row text-black py-4 mt-5">
-          <div className="col-8">{description}</div>
-          <div className="col-4 d-flex justify-content-center align-items-center">
-            <a href={instaMeu} target="_blank" rel="noreferrer">
-              <Styled.InstaIcon />
-            </a>
-            <a href={github} target="_blank" rel="noreferrer">
-              <Styled.GitHubIcon />
-            </a>
-            <a href={instaDela} target="_blank" rel="noreferrer">
-              <Styled.InstaIcon />
-            </a>
-          </div>
-          <div
-            className="col-12 d-flex justify-content-center"
-            style={{ marginTop: "30px" }}
-          >
-            {copyright}
+    <Container>
+      <Styled.Footer>
+        <div className="container">
+          <div className="row text-black py-4 mt-5">
+            <div className="col-8">{description}</div>
+            <div className="col-4 d-flex justify-content-center align-items-center">
+              <a href={instaMeu} target="_blank" rel="noreferrer">
+                <Styled.InstaIcon />
+              </a>
+              <a href={github} target="_blank" rel="noreferrer">
+                <Styled.GitHubIcon />
+              </a>
+              <a href={instaDela} target="_blank" rel="noreferrer">
+                <Styled.InstaIcon />
+              </a>
+            </div>
+            <div
+              className="col-12 d-flex justify-content-center"
+              style={{ marginTop: "30px" }}
+            >
+              {copyright}
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </Styled.Footer>
+    </Container>
   )
 }
 
--- src/components/Footer/styled.js
+++ src/components/Footer/styled.js
@@ -1,6 +1,16 @@
 import styled from "styled-components"
 import Icons from "../icons"
 
+export const Footer = styled.div`
+  background: #a9d0f5;
+  border-radius: 16px 16px 0px 0px;
+  width: 100%;
+  height: 185px;
+  position: relative;
+  bottom: 0;
+  left: 0;
+`
+
 export const GitHubIcon = styled(Icons.Github)`
   margin-right: 10px;
   width: 35px;
